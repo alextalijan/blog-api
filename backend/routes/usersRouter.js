de3db712aaa37e28c298bda797118697b7ee6804
alextalijan <acatalijan@icloud.com>
2025-10-08 17:14:02 -0500

    feat: write middleware for authenticating users

--- backend/routes/usersRouter.js
+++ backend/routes/usersRouter.js
@@ -3,7 +3,6 @@ const { Router } = require('express');
 const router = Router();
 const controller = require('../controllers/usersController');
 
-router.post('/', controller.addUser);
 router.get('/:userId', controller.getUser);
 router.put('/:userId/password', controller.updatePassword);
 
---
+++ backend/utils/isLoggedIn.js
@@ -0,0 +1,26 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+function isLoggedIn(req, res, next) {
+  const authHeader = req.headers.authorization;
+
+  if (authHeader && authHeader.startsWith('Bearer')) {
+    const token = authHeader.split(' ')[1];
+
+    // Check if the token is valid
+    jwt.verify(token, process.env.JWT_SECRET, (err, payload) => {
+      if (err) {
+        return res.status(401).json({ success: false, message: err.message });
+      }
+
+      req.user = {
+        id: payload.userID,
+      };
+      next();
+    });
+  } else {
+    res.status(401).json({ success: false, message: 'Missing authorization header.' });
+  }
+}
+
+module.exports = isLoggedIn;
