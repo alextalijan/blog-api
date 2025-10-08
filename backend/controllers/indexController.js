a4f24afeb2f8a8a9c45a7df27fc57c3fe1ab574a
alextalijan <acatalijan@icloud.com>
2025-10-08 16:15:23 -0500

    refactor: move adding users to /register route and return token

--- backend/controllers/indexController.js
+++ backend/controllers/indexController.js
@@ -1,5 +1,30 @@
 const { PrismaClient } = require('@prisma/client');
+const bcrypt = require('bcryptjs');
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
 
 const prisma = new PrismaClient();
 
-module.exports = {};
+module.exports = {
+  register: async (req, res) => {
+    const json = req.body;
+
+    const hash = await bcrypt.hash(json.password, 10);
+    const user = await prisma.user.create({
+      data: {
+        username: json.username,
+        hash,
+      },
+      select: {
+        id: true,
+      },
+    });
+
+    // Create a jwt token
+    const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, {
+      expiresIn: 7 * 24 * 60 * 60, // 7 days in seconds
+    });
+
+    res.json({ success: true, message: 'User created.', token });
+  },
+};
--- backend/routes/indexRouter.js
+++ backend/routes/indexRouter.js
@@ -3,4 +3,6 @@ const { Router } = require('express');
 const router = Router();
 const controller = require('../controllers/indexController');
 
+router.post('/register', controller.register);
+
 module.exports = router;
