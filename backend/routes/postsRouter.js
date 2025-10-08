7f2d32ee922e6ef32626fa7a47fb770080d4dc2f
alextalijan <acatalijan@icloud.com>
2025-10-08 17:35:28 -0500

    feat: add isLoggedIn authentication on necessary routes

--- backend/routes/postsRouter.js
+++ backend/routes/postsRouter.js
@@ -7,10 +7,10 @@ const controller = require('../controllers/postsController');
 router.get('/', controller.getPosts);
 router.post('/', isLoggedIn, controller.addPost);
 router.get('/:postId', controller.getPost);
-router.put('/:postId', controller.updatePost);
-router.delete('/:postId', controller.deletePost);
+router.put('/:postId', isLoggedIn, controller.updatePost);
+router.delete('/:postId', isLoggedIn, controller.deletePost);
 router.get('/:postId/comments', controller.getComments);
-router.post('/:postId/comments', controller.addComment);
-router.delete('/postId/comments/:commentId', controller.deleteComment);
+router.post('/:postId/comments', isLoggedIn, controller.addComment);
+router.delete('/postId/comments/:commentId', isLoggedIn, controller.deleteComment);
 
 module.exports = router;
