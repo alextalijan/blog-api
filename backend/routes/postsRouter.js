7d828463e23697d653e2c7fa26677a6fc96a88d4
alextalijan <acatalijan@icloud.com>
2025-10-07 23:17:13 -0500

    feat: add all GET routes for posts and comments

--- backend/routes/postsRouter.js
+++ backend/routes/postsRouter.js
@@ -4,5 +4,8 @@ const router = Router();
 const controller = require('../controllers/postsController');
 
 router.get('/', controller.postsGet);
+router.get('/:postId', controller.postGet);
+router.get('/:postId/comments', controller.postCommentsGet);
+router.get('/:postId/comments/commentId', controller.postCommentGet);
 
 module.exports = router;
