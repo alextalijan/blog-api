f8f8bd2dfb2859b7383e6a5da9910c410a98031a
alextalijan <acatalijan@icloud.com>
2025-10-08 16:21:35 -0500

    feat: add route for logging in

--- backend/controllers/indexController.js
+++ backend/controllers/indexController.js
@@ -27,4 +27,29 @@ module.exports = {
 
     res.json({ success: true, message: 'User created.', token });
   },
+  login: async (req, res) => {
+    const json = req.body;
+
+    const user = await prisma.user.findUnique({
+      where: {
+        username: json.username,
+      },
+    });
+
+    // Verify user
+    if (!user) {
+      return res.status(404).json({ success: false, message: 'User does not exist.' });
+    }
+    const passwordMatch = await bcrypt.compare(json.password, user.hash);
+    if (!passwordMatch) {
+      return res.status(401).json({ success: false, message: 'Password is incorrect.' });
+    }
+
+    // Generate jwt token
+    const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, {
+      expiresIn: 7 * 24 * 60 * 60, // 7 days in seconds
+    });
+
+    res.json({ success: true, message: 'Logged in.', token });
+  },
 };
--- backend/routes/indexRouter.js
+++ backend/routes/indexRouter.js
@@ -4,5 +4,6 @@ const router = Router();
 const controller = require('../controllers/indexController');
 
 router.post('/register', controller.register);
+router.post('/login', controller.login);
 
 module.exports = router;
