a9d06fdf2b3b484b51aedd87cc375348f70a7e23
alextalijan <acatalijan@icloud.com>
2025-10-07 23:20:45 -0500

    feat: write GET routes for users

--- backend/routes/usersRouter.js
+++ backend/routes/usersRouter.js
@@ -3,4 +3,7 @@ const { Router } = require('express');
 const router = Router();
 const controller = require('../controllers/usersController');
 
+router.get('/', controller.usersGet);
+router.get('/users/:userId', controller.userGet);
+
 module.exports = router;
