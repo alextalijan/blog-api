4e999f9dbf8ae0b2e7fc2458d06666a20a6bf284
alextalijan <acatalijan@icloud.com>
2025-10-09 17:44:17 -0500

    style: create layout and style for header

--- frontend/read-blog/src/components/Header/Header.jsx
+++ frontend/read-blog/src/components/Header/Header.jsx
@@ -1,21 +1,29 @@
 import { Link } from 'react-router-dom';
+import styles from './Header.module.css';
 
 function Header() {
   return (
     <header>
       <nav>
-        <ul>
+        <ul className={styles.links}>
           <li>
-            <Link to="/">Home</Link>
+            <Link to="/" className={styles.link}>
+              Home
+            </Link>
           </li>
           <li>
-            <Link to="/account">My Account</Link>
+            <Link to="/account" className={styles.link}>
+              My Account
+            </Link>
           </li>
           <li>
-            <button type="button">Log Out</button>
+            <button type="button" className={styles.link}>
+              Log Out
+            </button>
           </li>
         </ul>
       </nav>
+      <hr />
     </header>
   );
 }
