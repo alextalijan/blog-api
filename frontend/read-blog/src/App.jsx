280d0d59178af03a40ceee80685e3552c4c521d8
alextalijan <acatalijan@icloud.com>
2025-10-09 15:36:31 -0500

    feat: create router in App to show different pages

--- frontend/read-blog/src/App.jsx
+++ frontend/read-blog/src/App.jsx
@@ -1,62 +1,17 @@
-import { useEffect, useState } from 'react';
-import Post from './components/Post/Post';
-import './App.css';
-import ModalPost from './components/ModalPost/ ModalPost';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './components/HomePage/HomePage';
+import LoginPage from './components/LoginPage/LoginPage';
+import AccountPage from './components/AccountPage/AccountPage';
 
 function App() {
-  const [posts, setPosts] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-  const [activePostId, setActivePostId] = useState(null);
-
-  useEffect(() => {
-    fetch('http://localhost:3000/posts')
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error();
-        }
-
-        return response.json();
-      })
-      .then((data) => {
-        setPosts(data);
-      })
-      .catch((error) => {
-        console.error(error);
-        setError(new Error('Failed to fetch posts. Please try again.'));
-      })
-      .finally(() => {
-        setLoading(false);
-      });
-  }, []);
-
   return (
-    <>
-      <h1 className="h1">Posts</h1>
-      {error && <p className="error">{error.message}</p>}
-      {loading ? (
-        <p className="loading">Loading...</p>
-      ) : (
-        <div className="posts">
-          {posts.map((post) => {
-            return (
-              <Post
-                key={post.id}
-                title={post.title}
-                text={post.text}
-                date={post.updatedAt}
-                author={post.author.username}
-                numOfComments={post._count.comments}
-                handleClick={() => setActivePostId(post.id)}
-              />
-            );
-          })}
-        </div>
-      )}
-      {activePostId && (
-        <ModalPost postId={activePostId} handleClick={() => setActivePostId(null)} />
-      )}
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/account" element={<AccountPage />} />
+      </Routes>
+    </Router>
   );
 }
 
