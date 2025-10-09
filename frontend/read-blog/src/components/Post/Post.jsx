0f442c87d158fb5f086a22734ea259c48fa2ac80
alextalijan <acatalijan@icloud.com>
2025-10-08 19:47:35 -0500

    feat: display post date in a prettier format

--- frontend/read-blog/src/components/Post/Post.jsx
+++ frontend/read-blog/src/components/Post/Post.jsx
@@ -1,10 +1,12 @@
+import prettifyDate from '../../utils/prettifyDate';
+
 function Post({ title, text, date, author, numOfComments }) {
   return (
     <div className="card">
       <h2>{title}</h2>
       <p>{text}</p>
       <span>Author: {author}</span>
-      <span>Posted on {date}</span>
+      <span>Posted on {prettifyDate(date)}</span>
       <span>{numOfComments} Comments</span>
     </div>
   );
