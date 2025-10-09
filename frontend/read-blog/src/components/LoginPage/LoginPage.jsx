ecd8f4bda3454bd2bb81e1027491981c1f110224
alextalijan <acatalijan@icloud.com>
2025-10-09 15:49:22 -0500

    feat: write content for LoginPage

--- frontend/read-blog/src/components/LoginPage/LoginPage.jsx
+++ frontend/read-blog/src/components/LoginPage/LoginPage.jsx
@@ -1,5 +1,24 @@
+import styles from './LoginPage.module.css';
+
 function LoginPage() {
-  return;
+  return (
+    <>
+      <h1 className={styles.h1}>Log In</h1>
+      <form action="" method="POST" className={styles.form}>
+        <div className={styles.field}>
+          <label htmlFor="username">Username:</label>
+          <input type="text" name="username" id="username" className={styles.input} />
+        </div>
+        <div className={styles.field}>
+          <label htmlFor="password">Password:</label>
+          <input type="password" name="password" id="password" className={styles.input} />
+        </div>
+        <button type="submit" className={styles['login-btn']}>
+          Log In
+        </button>
+      </form>
+    </>
+  );
 }
 
 export default LoginPage;
