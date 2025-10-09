48e097c7e1c27bfd568c0214e5b5ad9a2025bec0
alextalijan <acatalijan@icloud.com>
2025-10-08 19:20:44 -0500

    feat: configure cors so frontend app can fetch data

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 require('dotenv').config();
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -10,6 +11,9 @@ const usersRouter = require('./routes/usersRouter');
 
 // Allow the app to parse incoming JSON objects in request
 app.use(express.json());
+
+// Allow cross origin resource sharing
+app.use(cors());
 
 app.use('/', indexRouter);
 app.use('/posts', postsRouter);
