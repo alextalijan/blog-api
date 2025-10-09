217d2edec251dee0dcc0bc7f0e8ee69420707643
alextalijan <acatalijan@icloud.com>
2025-10-08 23:07:33 -0500

    style: create layout and look of post cards

--- frontend/read-blog/src/components/Post/Post.jsx
+++ frontend/read-blog/src/components/Post/Post.jsx
@@ -1,13 +1,18 @@
 import prettifyDate from '../../utils/prettifyDate';
+import styles from './Post.module.css';
 
 function Post({ title, text, date, author, numOfComments }) {
   return (
-    <div className="card">
-      <h2>{title}</h2>
-      <p>{text}</p>
-      <span>Author: {author}</span>
-      <span>Posted on {prettifyDate(date)}</span>
-      <span>{numOfComments} Comments</span>
+    <div className={styles.card}>
+      <h2 className={styles.title}>{title}</h2>
+      <span className={styles.author}>Author: {author}</span>
+      <p className={styles.text}>{text}</p>
+      <div className={styles.footer}>
+        <span className={styles.date}>Posted on {prettifyDate(date)}</span>
+        <button type="button" className={styles['comments-btn']}>
+          {numOfComments} Comments
+        </button>
+      </div>
     </div>
   );
 }
