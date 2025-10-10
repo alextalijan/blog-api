18f3e082a7aa7e575a5ddefaf38ec638692ce58d
alextalijan <acatalijan@icloud.com>
2025-10-09 21:17:08 -0500

    feat: add page content for AccountPage for changing password

--- frontend/read-blog/src/components/AccountPage/AccountPage.jsx
+++ frontend/read-blog/src/components/AccountPage/AccountPage.jsx
@@ -1,5 +1,25 @@
+import { useContext } from 'react';
+import { UserContext } from '../../App';
+
 function AccountPage() {
-  return;
+  const { user } = useContext(UserContext);
+
+  function handlePasswordChange(event) {}
+
+  return (
+    <>
+      <h1>My Account</h1>
+      <p>Welcome back {user.username}</p>
+      <h2>Change Password</h2>
+      <form action="" method="PUT" onSubmit={handlePasswordChange}>
+        <label htmlFor="password">New Password:</label>
+        <input type="password" name="password" id="password" />
+        <label htmlFor="passwordConfirmation">Confirm Password:</label>
+        <input type="password" name="passwordConfirmation" id="passwordConfirmation" />
+        <button type="submit">Change</button>
+      </form>
+    </>
+  );
 }
 
 export default AccountPage;
